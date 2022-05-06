e8dcd538ba63032ea7367d1a3e256c19efa9071a
Alex777Russia <shevalex777@mail.ru>
2022-05-06 13:24:56 +0300

    Запускается доска

--- src/main/java/com/example/chess/HelloApplication.java
+++ src/main/java/com/example/chess/HelloApplication.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
+        FXMLLoader Chess = new FXMLLoader(HelloApplication.class.getResource("field.fxml"));
+        Scene scene = new Scene(Chess.load());
+        stage.setTitle("Hello");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
