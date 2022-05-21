5fd43a39024ee03ed552c76f53d37a7703e47987
Alex777Russia <shevalex777@mail.ru>
2022-05-22 00:43:20 +0300

    Ферзь и пешка исправлены

--- src/main/java/com/example/chess/Models/Figures/Queen.java
+++ src/main/java/com/example/chess/Models/Figures/Queen.java
@@ -42,7 +42,66 @@ public class Queen implements Figure {
 
     @Override
     public boolean canMakeMove(int row, int column) {
-        return true;
+        if (this.row == row && this.column != column) {
+            if (this.column - column > 0) {
+                for (int i = column + 1; i < this.column; ++i) {
+                    if (this.field.get(row).get(i) != null) {
+                        return false;
+                    }
+                }
+            } else {
+                for (int i = this.column + 1; i < column; ++i) {
+                    if (this.field.get(row).get(i) != null) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else if (this.column == column && this.row != row) {
+            if (this.row - row > 0) {
+                for (int i = row; i < this.row; ++i) {
+                    if (this.field.get(i).get(row) != null) {
+                        return false;
+                    }
+                }
+            } else {
+                for (int i = this.row; i < row; ++i) {
+                    if (this.field.get(i).get(row) != null) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else if (this.row - row == this.column - column && this.row - row > 0) {
+           for (int i = 0; i < this.row - row - 1; ++i) {
+               if (this.field.get(row + i).get(column + i) != null) {
+                   return false;
+               }
+           }
+           return true;
+        } else if (this.row - row == this.column - column && this.row - row < 0) {
+            for (int i = 0; i < row - this.row - 1; ++i) {
+                if (this.field.get(this.row + i).get(this.column + i) != null) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (this.row - row == column - this.column && this.row - row > 0) {
+            for (int i = 0; i < this.row - row - 1; ++i) {
+                if (this.field.get(row + i).get(this.column + i) != null) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (this.row - row == column - this.column && this.row - row < 0) {
+            for (int i = 0; i < row - this.row - 1; ++i) {
+                if (this.field.get(this.row + i).get(column + i) != null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     @Override
